29d95b6aa6e2fd3ba3a93d9dba95cc83b2a1c953
ChenJianPing <2674950738@qq.com>
2020-07-09 15:18:46 +0800

    微信小程序分支提交

--- SSH/src/main/java/com/bigjava/dao/BaseDAO.java
+++
@@ -1,7 +0,0 @@
-package com.bigjava.dao;
-
-import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
-
-public class BaseDAO extends HibernateDaoSupport {
-
-}
--- SSH/src/main/java/com/bigjava/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.bigjava.dao;
-
-public interface UserDAO {
-
-	public boolean login(String username, String password);
-
-}
